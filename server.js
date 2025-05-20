4dbd8d9f52fc52bb9aed12c955120a157bd2b706
sykrwasd <umarsyakir16@gmail.com>
2025-05-20 13:08:43 +0800

    change sumn

--- server.js
+++ server.js
@@ -62,4 +62,5 @@ app.post('/updateProgress', async (req, res) => {
 
 
 
-  app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
